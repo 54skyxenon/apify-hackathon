84825e688d29ff124023e6d2ca5d5f86572b9c9f
Brandon Liang <54skyxenon@gmail.com>
2021-10-16 17:22:28 +0200

    Remove unused example imports

--- example.py
+++ example.py
@@ -1,9 +1,8 @@
 #!/usr/bin/env python3
 
 from apify_client import ApifyClient
-import urllib.request as u2
-from lxml import html
-import os, json
+import os
+import json
 
 API_TOKEN = os.getenv('APIFY_TOKEN')
 APIFY_DEFAULT_KEY_VALUE_STORE_ID = os.getenv('APIFY_DEFAULT_KEY_VALUE_STORE_ID')
@@ -14,8 +13,3 @@ json_record = kv_store.get_record('INPUT')
 
 print('Input received:')
 print(json_record)
-
-request = u2.Request('https://example.com/')
-src = u2.urlopen(request).read()
-html_load = html.fromstring(src.decode())
-print( html_load.xpath('//h1/text()')[0] )

79f42619681e01a2f752825b86dd2c74cd93e358
Brandon Liang <54skyxenon@gmail.com>
2021-10-16 15:55:31 +0200

    initial commit

---
+++ example.py
@@ -0,0 +1,17 @@
+#! /usr/bin/env python3
+
+import urllib.request as u2
+from lxml import html
+import os, json
+
+print( os.environ )
+
+request = u2.Request('https://example.com/')
+
+src = u2.urlopen(request).read()
+
+html_load = html.fromstring( src.decode() )
+
+print( html_load.xpath('//h1/text()')[0] )
+
+

d4258d7fe145999ff4c26c88d29fd155b08fabe4
Brandon Liang <54skyxenon@gmail.com>
2021-10-16 17:53:53 +0200

    Experiment with dataset

--- example.py
+++ example.py
@@ -6,15 +6,15 @@ import json
 
 API_TOKEN = os.getenv('APIFY_TOKEN')
 APIFY_DEFAULT_KEY_VALUE_STORE_ID = os.getenv('APIFY_DEFAULT_KEY_VALUE_STORE_ID')
+APIFY_DEFAULT_DATASET_ID = os.getenv('APIFY_DEFAULT_DATASET_ID')
 
 apify_client = ApifyClient(token=API_TOKEN)
 kv_store = apify_client.key_value_store(APIFY_DEFAULT_KEY_VALUE_STORE_ID)
+dataset = apify_client.dataset(APIFY_DEFAULT_DATASET_ID)
 json_input_record = kv_store.get_record('INPUT')
 
 print('Input received:')
 print(json_input_record)
 
-kv_store.set_record('OUTPUT', {'foo': 'bar'})
-
-print('Output sent:')
-print(kv_store.get_record('OUTPUT'))
+print('Dataset')
+print(dataset.update({'foo': 'bar'}))

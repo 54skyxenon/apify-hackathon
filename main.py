071c780009d091999f4f42ae1a1d0ae0c2fa163e
Brandon Liang <54skyxenon@gmail.com>
2021-10-17 01:53:53 +0200

    Rough draft done, need to fix a lot

--- main.py
+++ main.py
@@ -4,7 +4,6 @@ from apify_client import ApifyClient
 from scraper import SignLanguageScraper
 import os
 import base64
-import json
 
 ### Load actor environment variables
 APIFY_TOKEN = os.getenv('APIFY_TOKEN')
@@ -22,13 +21,12 @@ print(str(type(json_input_record)) + ': ' + str(json_input_record))
 ### Call the sign language scraper
 scraper = SignLanguageScraper(json_input_record['language'], json_input_record['word'])
 
+### Get base64 representations of letter images in word spelling to render
 letter_images_base64 = []
-
-### TODO: implement get spelling images
-# for letter_file_path in scraper.get_spelling_images():
-#     with open(letter_file_path, "rb") as image_file:
-#         encoded_letter_image = base64.b64encode(image_file.read())
-#         images_base64.append(encoded_letter_image)
+for letter_file_path in scraper.get_spelling_images():
+    with open(letter_file_path, "rb") as image_file:
+        encoded_letter_image = base64.b64encode(image_file.read())
+        letter_images_base64.append(encoded_letter_image)
 
 kv_store.set_record('OUTPUT', {'video_url': scraper.get_video_url(), 'images_base64': letter_images_base64})
 
--- scraper.py
+++ scraper.py
@@ -13,11 +13,6 @@ from webdriver_manager.chrome import ChromeDriverManager
 
 MAX_DELAY_SECS = 4
 VIDEO_ENDPOINT = 'https://www.spreadthesign.com/isl.intl/search/'
-IMAGE_ENDPOINTS = {
-    'English (United States)': 'us',
-    'German (Germany)': 'de',
-    'Czech': 'cz'
-}
 
 CHROME_OPTIONS = Options()
 CHROME_OPTIONS.add_argument('--headless')
@@ -26,7 +21,7 @@ CHROME_OPTIONS.add_argument('--disable-dev-shm-usage')
 
 class SignLanguageScraper:
     ''' Scraper API class. '''
-    
+
     def __init__(self, language: str, word: str) -> None:
         self.language = language
         self.word = word
@@ -35,7 +30,7 @@ class SignLanguageScraper:
         ''' Gets a video of our word being read in a specific language. 
             Returns `None` if video translation is unavailable. '''
         
-        driver = webdriver.Chrome(ChromeDriverManager().install(), chrome_options=CHROME_OPTIONS)
+        driver = webdriver.Chrome(ChromeDriverManager().install(), options=CHROME_OPTIONS)
         driver.get(VIDEO_ENDPOINT)
 
         ## search for our word
@@ -62,13 +57,70 @@ class SignLanguageScraper:
 
     def get_spelling_images(self) -> List[str]:
         ''' Maps the spelling of a word to a sequence of image URLs. '''
-        pass
+        if self.language == 'English (United States)':
+            return self._american_spelling()
+        elif self.language == 'German (Germany)':
+            return self._german_spelling()
+        elif self.language == 'Czech':
+            return self._czech_spelling()
+        else:
+            return []
+
+    def _american_spelling(self) -> List[str]:
+        ''' Retrieves US sign language spelling of word. '''
+        image_paths = []
+
+        for letter in self.word.lower():
+            if letter:
+                image_paths.append(f'assets/us/{letter}.png')
+
+        return image_paths
+
+    def _german_spelling(self) -> List[str]:
+        ''' Retrieves German sign language spelling of word. '''
+        image_paths = []
+        transformed = self.word.lower().replace('sch', '^')
+        
+        for letter in transformed:
+            if letter:
+                image_paths.append(f'assets/de/{letter}.png')
+
+        return image_paths
+
+    def _czech_spelling(self) -> List[str]:
+        ''' Retrieves Czech sign language spelling of word. '''
+        image_paths = []
+        
+        ## detect length marked + hooked characters in Czech alphabet
+        ## á, č, ď, é, ě, í, ň, ó, ř, š, ť, ú, ů, ý, ž
+        length_marks = {'á': 'a', 'é': 'e', 'í': 'i', 'ó': 'o', 'ú': 'u', 'ů': 'u', 'ý': 'y'}
+        hooks = {'č': 'c', 'ď': 'd', 'ě': 'e', 'ň': 'n', 'ř': 'r', 'š': 's', 'ť': 't', 'ž': 'z'}
+
+        transformed = self.word.lower().replace('ch', '^')
+
+        for letter in transformed:
+            if letter:
+                if letter in length_marks:
+                    image_paths.append(f'assets/cz/{length_marks[letter]}.png')
+                    image_paths.append(f'assets/cz/length-mark.png')
+                elif letter in hooks:
+                    image_paths.append(f'assets/cz/{hooks[letter]}.png')
+                    image_paths.append(f'assets/cz/hook.png')
+                else:
+                    image_paths.append(f'assets/cz/{letter}.png')
+
+        return image_paths
 
 ### Tests
 if __name__ == "__main__":
     sl1 = SignLanguageScraper('English (United States)', 'university')
     print(sl1.get_video_url())
+    print(sl1.get_spelling_images())
+
     sl2 = SignLanguageScraper('Czech', 'university')
     print(sl2.get_video_url())
+    print(sl2.get_spelling_images())
+
     sl3 = SignLanguageScraper('German (Germany)', 'university')
     print(sl3.get_video_url())
+    print(sl3.get_spelling_images())

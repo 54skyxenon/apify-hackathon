04f19bd355da16da21e1d1e233a4470a3f4fa56d
Brandon Liang <54skyxenon@gmail.com>
2021-10-16 17:41:38 +0200

    Test printing output

--- example.py
+++ example.py
@@ -9,7 +9,12 @@ APIFY_DEFAULT_KEY_VALUE_STORE_ID = os.getenv('APIFY_DEFAULT_KEY_VALUE_STORE_ID')
 
 apify_client = ApifyClient(token=API_TOKEN)
 kv_store = apify_client.key_value_store(APIFY_DEFAULT_KEY_VALUE_STORE_ID)
-json_record = kv_store.get_record('INPUT')
+json_input_record = kv_store.get_record('INPUT')
 
 print('Input received:')
-print(json_record)
+print(json_input_record)
+
+json_output_record = kv_store.get_record('OUTPUT')
+
+print('Output sent:')
+print(json_output_record)
